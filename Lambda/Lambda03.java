0d6d98053519d59a4eb79602616866167db8a9b4
Ferhat <fcetinerr@gmail.com>
2021-12-29 00:00:47 +0300

    04. gun 1

--- Lambda/Lambda03.java
+++ Lambda/Lambda03.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class Lambda03 {
 
 	public static void main(String[] args) {
-		   List<String> list = new ArrayList<>(Arrays.asList("Mehmet","emre","nilgun","yýldýx","kader","emine","islam","islam","emre"));
+		 List<String> list = new ArrayList<String>(Arrays.asList("Mehmet", "emre", "nilgun", "yýldýx", "kader", "emine", "islam", "islam", "emre"));
 
 	        buyukHarfTekrarsizSira(list);
 	        System.out.println();
@@ -35,27 +35,34 @@ public class Lambda03 {
 	        System.out.println();
 	        System.out.println(" *** ");
 	        karakterSayýsýEnBuyuk(list);
+	        System.out.println();
+	        System.out.println(" *** ");
+	        karakterSayýsýEnBuyuk2(list);
+	        System.out.println();
+	        System.out.println(" *** ");
+	        ilkElemanHaricSonHarfSýralý(list);
 
 	    }
+
 	    //List elemanlarini alafabetik buyuk harf ve  tekrarsiz yazdiriniz
-	    public static void buyukHarfTekrarsizSira(List<String> list){
+	    public static void buyukHarfTekrarsizSira(List<String> list) {
 	        list.
 	                stream().//akisa girdi
 	                // map(t->t.toUpperCase()).//elelmanlar byk harf update edildi
 	                        map(String::toUpperCase).//elelmanlar byk harf update edildi
 	                sorted().//alfabetik sira
 	                distinct().//tekrarsiz  yapildi
-	                forEach(t->System.out.print(t + " "));//yazdirilidi
+	                forEach(t -> System.out.print(t + " "));//yazdirilidi
 	        //EMRE EMÝNE KADER MEHMET NÝLGUN YILDIZ ÝSLAM
 
 
 	    }
 
 	    //list elelmanlarinin character sayisini ters sirali olarak tekrarsiz yazdiriniz
-	    public static void karakterSayisiTekrarsizTersSirali(List<String> list){
+	    public static void karakterSayisiTekrarsizTersSirali(List<String> list) {
 	        list.
 	                stream().
-	                map(t-> t.length()).//String data karakter sayýsýna update edildi
+	                map(t -> t.length()).//String data karakter sayýsýna update edildi
 	                sorted(Comparator.reverseOrder()).//ters sýra
 	                distinct().//tekrarsýz
 	                forEach(Lambda01::printEl);//yazdýrýldý
@@ -63,37 +70,37 @@ public class Lambda03 {
 
 	    //List elemanlarini character sayisina gore kckten byk egore yazdiriniz.
 
-	    public static void karakterSayisiSiraliEl(List<String> list){
+	    public static void karakterSayisiSiraliEl(List<String> list) {
 	        list.
 	                stream().
-	                sorted(Comparator.comparing(t->t.length())).//Karakter sayýsýna göre özel sýralama yapýldý
-	                forEach(t-> System.out.print(t+" "));
+	                sorted(Comparator.comparing(t -> t.length())).//Karakter sayýsýna göre özel sýralama yapýldý
+	                forEach(t -> System.out.print(t + " "));
 	    }
 
 	    //list elelmanlarinin son harfine gore ters sirali yazdiriniz
-	    public static void sonHarfineTersSýralýEl(List<String> list){
-	    list.
-	            stream().
-	            sorted(Comparator.//Karþýlaþtýrma methodlarýnýn oldugu class
-	                    comparing(t-> t.toString().
-	                            charAt(t.toString().length()-1)).//son indexe karþýlýk gelen harf
-	                    reversed()).//Son indexe gore ters sýralar
-	            forEach(t-> System.out.print(t+" "));
-	}
-
-	    //uzunlugu cift sayi olan elemanlarin uzunluklarinin karelerini hesaplayan, tekrarsiz buyukten kucuge sirali yazdiriniz.
-	    public static void ciftKareTekrarsizTersSýrali(List<String> list){
+	    public static void sonHarfineTersSýralýEl(List<String> list) {
 	        list.
 	                stream().
-	                map(t-> t.length()*t.length()).
+	                sorted(Comparator.//Karþýlaþtýrma methodlarýnýn oldugu class
+	                        comparing(t -> t.toString().
+	                        charAt(t.toString().length() - 1)).//son indexe karþýlýk gelen harf
+	                        reversed()).//Son indexe gore ters sýralar
+	                forEach(t -> System.out.print(t + " "));
+	    }
+
+	    //uzunlugu cift sayi olan elemanlarin uzunluklarinin karelerini hesaplayan, tekrarsiz buyukten kucuge sirali yazdiriniz.
+	    public static void ciftKareTekrarsizTersSýrali(List<String> list) {
+	        list.
+	                stream().
+	                map(t -> t.length() * t.length()).
 	                filter(Lambda01::ciftBul).
 	                distinct().
 	                sorted(Comparator.reverseOrder()).
 	                forEach(Lambda01::printEl);
 	    }
-	//List elelmmalarinin karakter sayisini 7 ve 7 'den az olma durumunu kontrol ediniz
 
-	    public static void harfSayisi7Kontrol (List<String> list){
+	    //List elelmmalarinin karakter sayisini 7 ve 7 'den az olma durumunu kontrol ediniz
+	    public static void harfSayisi7Kontrol(List<String> list) {
 	    /*  boolean kontrol= list.
 	              stream().
 	              allMatch(t-> t.length()<=7);//her bir elemaný harf sayýsýn <= eþleþmesine bak
@@ -106,22 +113,23 @@ public class Lambda03 {
 
 	        System.out.println(list.
 	                stream().
-	                allMatch(t -> t.length() <= 7)?
-	                "List elemanlarý 7 harfden buyuk deðil":
+	                allMatch(t -> t.length() <= 7) ?
+	                "List elemanlarý 7 harfden buyuk deðil" :
 	                "List elemanlarý 7 den büyük");
 	    }
+
 	    //List elelmanlarinin "W" ile baslamasýný kontrol ediniz
-	    public static void wIleBaslayanEl(List<String> list){
-	        System.out.println( list.
+	    public static void wIleBaslayanEl(List<String> list) {
+	        System.out.println(list.
 	                stream().
-	                noneMatch(t-> t.toLowerCase().startsWith("w"))?"w ile baþlayan isim yok":"w ile baþlayan isim var");
+	                noneMatch(t -> t.toLowerCase().startsWith("w")) ? "w ile baþlayan isim yok" : "w ile baþlayan isim var");
 
 	    }
 
-	    public static void xBitmeKontrol(List<String> list){
-	        System.out.println( list.
+	    public static void xBitmeKontrol(List<String> list) {
+	        System.out.println(list.
 	                stream().
-	                anyMatch(t-> t.endsWith("x"))?"x ile biten isim var":"x ile biten isim yok");
+	                anyMatch(t -> t.endsWith("x")) ? "x ile biten isim var" : "x ile biten isim yok");
 
 	        //  noneMatch(t -> t.startsWith("w"))--->  "hicbiri W ile baslamiyor" : "birileri W ile basliyor"
 	//  allMatch(t -> t.startsWith("w")) --->  "hepsi W ile basliyor" : "bir tanesi W ile baslamiyor"
@@ -130,11 +138,39 @@ public class Lambda03 {
 	    }
 
 	    //Karakter sayýsý en büyük elemaný yazdýrýn
-	    public static void karakterSayýsýEnBuyuk(List<String> list){
+	    public static void karakterSayýsýEnBuyuk(List<String> list) {
 	        System.out.println(list.
 	                stream().
 	                sorted(Comparator.comparing(t -> t.toString().length()).//length karakter uzunluguna gore sýraladý
 	                        reversed()).//ters sýraladý
-	                findFirst());//ilk elemaný aldý
+	                      findFirst());//ilk elemaný aldý
+	         }
+
+	    public static void karakterSayýsýEnBuyuk2(List<String> list) {
+	      Stream<String> sonIsým= list.
+	                stream().
+	                sorted(Comparator.comparing(t -> t.toString().length()).//length karakter uzunluguna gore sýraladý
+	                        reversed()).//ters sýraladý
+	                        limit(1); //limit(a) Akýþtan çýkan elemanlarý a parametresine gore limitler
+	        //limit methodundan çýkan veriler datadýr bu yuzden ham verinin iþlenmesi gerekir. Bunu Liste çeviririz ve kullanabiliriz
+	        //Bir magazanýn 100 musterisinden en fazla alýþveriþ yapan 5 muþterisi alýnabilir ve iþleme devam edilebilir
+	        System.out.println(Arrays.toString(sonIsým.toArray()));
 	    }
+
+	    //list elemanlarini son harfine göre siralayýp ilk eleman hariç kalan elemanlari yazdiniz
+
+	    public static void ilkElemanHaricSonHarfSýralý (List<String> list){
+	        list.
+	                stream().
+	                sorted(Comparator.comparing(t->t.toString().
+	                        charAt(t.length()-1))).
+	                skip(1).//akiþtan çýkan elamalarýn 1. parametreyi alýr
+	                forEach(t-> System.out.print(t+" "));
+	    }
+
+
+
+
+
+
 	}
